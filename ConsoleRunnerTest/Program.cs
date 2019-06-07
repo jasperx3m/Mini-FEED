fcc4c7f892d3c90c1a062f011498592ccc042038
orilla-jasper <jasper.orilla915@gmail.com>
2019-06-07 10:21:21 +0800

    Committed DDD and Design Pattern Practical

--- ConsoleRunnerTest/Program.cs
+++ ConsoleRunnerTest/Program.cs
@@ -14,7 +14,6 @@ namespace ConsoleRunnerTest
         {
             SharePost sp = new SharePost();
 
-
             sp.OriginalPost("Aaron Custodio");
             sp.OriginalPost("Kyla Calpito");
             sp.ClonePost(0,"Charles kenichi");
